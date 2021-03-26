5937655a265327a7e1814a2b141a9710a15a8fed
ajburns651 <ajburns651@gmail.com>
2021-03-26 08:58:22 -0700

    killvnc xmonad

--- DoomEmacs/.doom.d/config.el
+++ DoomEmacs/.doom.d/config.el
@@ -64,16 +64,29 @@
       (:after dired
        (:map dired-mode-map
         :desc "Peep-dired image previews" "d p" #'peep-dired
+        :desc "Create new file" "d c" #'find-file
         :desc "Dired view file" "d v" #'dired-view-file)))
 ;; Make 'h' and 'l' go back and forward in dired. Much faster to navigate the directory structure!
 (evil-define-key 'normal dired-mode-map
   (kbd "h") 'dired-up-directory
-  (kbd "l") 'dired-open-file) ; use dired-find-file instead if not using dired-open package
+  (kbd "l") 'dired-open-file
+  (kbd "<left>") 'dired-up-directory
+  (kbd "<right>") 'dired-open-file) ; use dired-find-file instead if not using dired-open package
 ;; If peep-dired is enabled, you will get image previews as you go up/down with 'j' and 'k'
 (evil-define-key 'normal peep-dired-mode-map
   (kbd "j") 'peep-dired-next-file
-  (kbd "k") 'peep-dired-prev-file)
+  (kbd "k") 'peep-dired-prev-file
+  (kbd "<up>") 'peep-dired-prev-file
+  (kbd "<down>") 'peep-dired-next-file)
 (add-hook 'peep-dired-hook 'evil-normalize-keymaps)
+;; With dired-open plugin, you can launch external programs for certain extensions
+;; For example, I set all .png files to open in 'sxiv' and all .mp4 files to open in 'mpv'
+(setq dired-open-extensions '(("gif" . "feh")
+                              ("jpg" . "feh")
+                              ("png" . "feh")
+                              ("pdf" . "brave")
+                              ("mkv" . "mpv")
+                              ("mp4" . "mpv")))
 
 ;; Email Config
 (setq user-mail-address "ajburns651@gmail.com"
@@ -95,3 +108,15 @@
         ("/gmail/Sent Items" . ?s)
         ("/gmail/Drafts"     . ?d)
         ("/gmail/Trash"      . ?t)))
+
+;; Prefer Vertical Split
+(defun prefer-horizontal-split ()
+  (set-variable 'split-height-threshold nil t)
+  (set-variable 'split-width-threshold 40 t)) ; make this as low as needed
+(add-hook 'markdown-mode-hook 'prefer-horizontal-split)
+(map! :leader
+      :desc "Clone indirect buffer other window" "b c" #'clone-indirect-buffer-other-window)
+
+;; Org bullets
+(after! org
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
